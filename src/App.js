ddc124a4f31f6b3f475058af8bdc21810f2f4a11
Tolci, Ionut <i.tolci@solarlux.com>
2023-01-22 02:15:06 +0200

    Set up a close-to-reality copy of the CoderByte challenge from @Acquco

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function ContactForm({ addNewContact }) {
+  const [contact, setContact] = useState({
+    userFirstName: "Coder",
+    userLastName: "Byte",
+    userPhone: "1234"
+  });
+
+  const handleContactChange = (event) => {
+    setContact({
+      ...contact,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleNewContact = (event) => {
+    event.preventDefault();
+    addNewContact(contact);
+  };
+
+  return (
+    <form onSubmit={handleNewContact}>
+      <input
+	name="userFirstName"
+	onChange={handleContactChange}
+	value={contact.userFirstName}
+      />
+      <input
+	name="userLastName"
+	onChange={handleContactChange}
+	value={contact.userLastName}
+      />
+      <input
+        name="userPhone"
+	onChange={handleContactChange}
+	value={contact.userPhone}
+      />
+      <input
+        type="submit"
+      />
+    </form>
+  );
+}
+
+function ContactsTable({ contacts }) {
+  const sortedContacts = [...contacts].sort(
+    (c1, c2) => {
+      if (c1.userLastName.toLowerCase() > c2.userLastName.toLowerCase()) {
+        return -1;
+      }
+      if (c1.userLastName.toLowerCase() > c2.userLastName.toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    }
+  );
+
+  return (
+    <table>
+      <thead>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Phone</th>
+      </thead>
+      <tbody>{
+	sortedContacts.map(({userFirstName, userLastName, userPhone}, idx) => (
+          <tr key={`${userFirstName}-${idx}`}>
+            <td>{userFirstName}</td>
+            <td>{userLastName}</td>
+            <td>{userPhone}</td>
+          </tr>
+	))
+      }</tbody>
+    </table>
+  );
+}
+
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const addNewContact = ({ userFirstName, userLastName, userPhone }) => {
+    setContacts([
+      ...contacts,
+      { userFirstName, userLastName, userPhone }
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactForm addNewContact={addNewContact} />
+      <ContactsTable contacts={contacts} />
     </div>
   );
 }
